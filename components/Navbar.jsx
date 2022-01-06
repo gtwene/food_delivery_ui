99e9a65ba50b769ae3834cb95639663bd34719c9
Kwame <gtwene001@st.ug.edu.gh>
2022-01-06 23:10:44 +0000

    init

---
+++ components/Navbar.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.items}>
+                <div className={styles.callButton}>
+                    <Image src="/img/telephone.png" alt="" width='32' height='32' />
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW!</div>
+                    <div className={styles.text}>055 072 4745</div>
+                </div>
+            </div>
+            <div className={styles.items}>center</div>
+            <div className={styles.items}>right</div>
+        </div>
+    )
+}
+
+export default Navbar
+
