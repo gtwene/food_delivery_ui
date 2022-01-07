e93ec9cd9cc8a326e4ed52f5025bc155b7c6dc4d
Kwame <gtwene001@st.ug.edu.gh>
2022-01-07 15:27:32 +0000

    CheckOut Completed

---
+++ pages/cart.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import styles from "../styles/Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extra</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/img/pizza.png"
+                  layout="fill"
+                  objectFit="cover"
+                  alt=""
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>CORALZO</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>GHS19.90</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>GHS39.80</span>
+            </td>
+          </tr>
+
+          
+          
+        </table>
+      </div>
+      <div className={styles.right}>
+          <div className={styles.wrapper}>
+              <h2 className={styles.title}>CART TOTAL</h2>
+              <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Subtotal:</b>GHS79.60
+              </div>
+              <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Discount:</b>GHS0.00
+              </div>
+              <div className={styles.totalText}>
+                  <b className={styles.totalTextTitle}>Total:</b>GHS79.60
+              </div>
+              <button className={styles.button}>CHECKOUT NOW!</button>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
