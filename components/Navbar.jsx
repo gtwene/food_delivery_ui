05157aa0caaeca997dade7a3d251e6256e95fb4c
Kwame <gtwene001@st.ug.edu.gh>
2022-01-07 09:16:05 +0000

    Navbar_completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,38 @@
-import Image from "next/image"
-import styles from "../styles/Navbar.module.css"
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <div className={styles.container}>
-                <div className={styles.callButton}>
-                    <Image src="/img/telephone.png" alt="" width='32' height='32' />
-                </div>
-            <div className={styles.items}>
-            
-                <div className={styles.texts}>
-                    <div className={styles.text}>ORDER NOW!</div>
-                    <div className={styles.text}>055 072 4745</div>
-                </div>
-            </div>
-            <div className={styles.items}>center</div>
-            <div className={styles.items}>right</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
         </div>
-    )
-}
 
-export default Navbar
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>055 072 4745</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+          <ul className={styles.list}>
+              <li className={styles.listItem}>Homepage</li>
+              <li className={styles.listItem}>Products</li>
+              <li className={styles.listItem}>Menu</li>
+              <Image src="/img/logo.png" alt="" width="160px" height="69px" />
+              <li className={styles.listItem}>Events</li>
+              <li className={styles.listItem}>Blog</li>
+              <li className={styles.listItem}>Contact</li>
+          </ul>
+      </div>
+      <div className={styles.item}>
+          <div className={styles.cart}>
+          <Image src="/img/blue-shopping-cart-png.png" alt="" width="30px" height="30px" />
+          <div className={styles.counter}>2</div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
+export default Navbar;
