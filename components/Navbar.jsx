758a6bb17f5dba7e1321f874e11e85a787afd228
Kwame <gtwene001@st.ug.edu.gh>
2022-01-07 08:34:01 +0000

    add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,11 @@ import styles from "../styles/Navbar.module.css"
 const Navbar = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.items}>
                 <div className={styles.callButton}>
                     <Image src="/img/telephone.png" alt="" width='32' height='32' />
                 </div>
+            <div className={styles.items}>
+            
                 <div className={styles.texts}>
                     <div className={styles.text}>ORDER NOW!</div>
                     <div className={styles.text}>055 072 4745</div>
